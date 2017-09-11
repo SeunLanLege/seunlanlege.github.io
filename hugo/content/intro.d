9cdc364d98d9b04c3839ce8bd555f487043539a1
seunlanlege <slanlege@c2gconsulting.com>
2017-09-11 23:09:22 +0100

    re - ordered

---
+++ hugo/content/intro.d
@@ -0,0 +1,8 @@
++++
+title = "Intro"
+weight = 10
+draft = false
++++
+
+{{< figure class="image main" src="/images/pic01.jpg" >}}
+Hi there, welcome to my website! You'll find my work experience here.
